1ecb46eed0788e5c8c8ea1fe756a5ed5de68ee5f
sontran22 <86516446+sontran22@users.noreply.github.com>
2022-05-19 22:30:14 +0700

    Add cs file

---
+++ Calculating.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using MailKit.Net.Pop3;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace NT106_M21_Group7_LyThuyet5_5
+{
+    public partial class DashBoard : Form
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Form sendMailForm = new SendMail();
+            sendMailForm.Show();
+        }
+
+        private void btnRecv_Click(object sender, EventArgs e)
+        {
+            Form recvMailForm = new RecvMail();
+            recvMailForm.Show();
+        }
+    }
+}
